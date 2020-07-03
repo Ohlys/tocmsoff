7af94ac25fb13e46f6a2f1f0b7ae7182fc82018c
Ohlys <67110425+Ohlys@users.noreply.github.com>
2020-07-03 13:24:14 +0200

    Update en.php

--- conf/en.php
+++ conf/en.php
@@ -15,3 +15,44 @@ $newsindexdiv = "Our last news";
 //
 //		register.php
 //
+
+$registerpagetitle = "Register";
+$registerpagediv = 'Register to '.Info("name");
+$buttontologin = "Login ->";
+$usernameregister = "Username : ";
+$passwordregister = "Password : ";
+$mailregister = "Your email adress : ";
+$submitbuttonregister = "Register";
+$lastusers = "The last registered";
+
+
+//
+//		me.php
+//
+
+$buttontologout = "Logout";
+$adminme = "Panel";
+$lastestmaj = "Update";
+$textedivmaj = "Update done by ";
+$gotoclient = "Go to the hotel";
+$titledivme = "Your profile";
+
+//
+//		Error Bubble
+//
+
+$errorannonce = "Error !";
+$successannonce = "Congratulations!";
+
+
+//
+//		navbar
+//
+
+$navonline = "Player online";
+$navstaffs = "Staffs online";
+$navprofil = "My profile";
+$navcommunity = "Community";
+$nav2staffs = "Our teams";
+$nav2news = "News";
+$nav2home = "Profile of players";
