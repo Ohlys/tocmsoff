f9134d7445a3225af1a6cd5710b01d346ad13cc4
Ohlys <67110425+Ohlys@users.noreply.github.com>
2020-07-03 13:24:35 +0200

    Update fr.php

--- conf/fr.php
+++ conf/fr.php
@@ -15,3 +15,42 @@ $newsindexdiv = "Notre dernier article";
 //
 //		register.php
 //
+
+$registerpagetitle = "Inscription";
+$registerpagediv = 'Inscription sur '.Info("name");
+$buttontologin = "Connexion ->";
+$usernameregister = "Pseudo : ";
+$passwordregister = "Mot de passe : ";
+$mailregister = "Votre adresse email : ";
+$submitbuttonregister = "M'inscrire";
+$lastusers = "Les derniers inscrits";
+
+//
+//		me.php
+//
+
+$buttontologout = "Déconnexion";
+$adminme = "Administration";
+$lastestmaj = "Compte rendus de la MAJ";
+$textedivmaj = "Mise à jour effectué par ";
+$gotoclient = "Accéder à l'hôtel";
+$titledivme = "Ton profil";
+
+//
+//		Bulle d'erreur
+//
+
+$errorannonce = "Erreur!";
+$successannonce = "Félicitations!";
+
+//
+//		Barre de navigation
+//
+
+$navonline = "Joueurs en ligne";
+$navstaffs = "Staffs en ligne";
+$navprofil = "Mon profil";
+$navcommunity = "Communauté";
+$nav2staffs = "Equipe des staffs";
+$nav2news = "Articles";
+$nav2home = "Profil des joueurs";
